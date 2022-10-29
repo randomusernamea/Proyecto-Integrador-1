90e1c608e4e9d8a66237e2bab4b1e150a991c6ee
randomusernamea <stars.a@outlook.com>
2022-10-28 21:50:36 -0300

    Algo hecho

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+noticias = document.getElementsByClassName('noticia');
+noticias[0].style.gridArea = '1 / 2 / span 1 / span 1';
+noticias[0].style.backgroundColor = 'red';
+
+noticias[1].style.gridArea = '1 / 3 / span 1 / span 1';
+noticias[1].style.backgroundColor = 'red';
+
+noticias[2].style.gridArea = '1 / 4 / span 1 / span 1';
+noticias[2].style.backgroundColor = 'red';
+
+noticias[3].style.gridArea = '1 / 5 / span 1 / span 1';
+noticias[3].style.backgroundColor = 'red';
+
+noticias[4].style.visibility = "hidden";
+noticias[4].style.backgroundColor = 'red';
+
+noticias[5].style.visibility = "hidden";
+noticias[5].style.backgroundColor = 'red';
