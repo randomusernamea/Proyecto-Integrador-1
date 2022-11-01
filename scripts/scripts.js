f9e8626dee6d7390fa310bab04b5c876c378a626
randomusernamea <stars.a@outlook.com>
2022-10-31 21:10:43 -0300

    Agrego botones para cambiar noticias

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,12 @@
 
 
 noticias = document.getElementsByClassName('noticia');
+agregarFechas();
+noticias[0].style.visibility = "visible";
 noticias[0].style.gridArea = '1 / 2 / span 1 / span 1';
 noticias[0].style.margin = '0 auto';
 
+noticias[1].style.visibility = "visible";
 noticias[1].style.gridArea = '1 / 3 / span 1 / span 1';
 noticias[1].style.margin = '0 auto';
 
@@ -15,20 +18,23 @@ noticias[3].style.gridArea = '1 / 5 / span 1 / span 1';
 noticias[3].style.margin = '0 auto';
 
 noticias[4].style.visibility = "hidden";
+noticias[4].style.gridArea = '1 / 6 / span 1 / span 1';
 noticias[4].style.margin = '0 auto';
 
 noticias[5].style.visibility = "hidden";
+noticias[5].style.gridArea = '1 / 0 / span 1 / span 1';
 noticias[5].style.margin = '0 auto';
 
 
-noticiasDivFecha = document.getElementsByClassName('noticiaFecha');
+function agregarFechas(){
+    noticiasDivFecha = document.getElementsByClassName('noticiaFecha');
 
-let i = 0;
-while (i<noticiasDivFecha.length){
-    agregarTiempoDesde(noticiasDivFecha[i]);
-    i++;
+    let i = 0;
+    while (i<noticiasDivFecha.length){
+        agregarTiempoDesde(noticiasDivFecha[i]);
+        i++;
+    }
 }
-
 function agregarTiempoDesde(element){
     fecha = element.getElementsByClassName('noticiaFechaTexto')
     tiempoDesde = element.getElementsByClassName('noticiaTiempoDesdeSubido');
@@ -62,4 +68,84 @@ function timeSince(date) {
     aux2 = new Date(date);
     toReturn = aux-aux2;
     return toReturn;
+}
+
+document.getElementById('button1').onclick =function() {switchNoticias(1)};
+document.getElementById('button2').onclick =function() {switchNoticias(2)};
+document.getElementById('button3').onclick =function() {switchNoticias(3)};
+
+
+function switchNoticias(number){
+    noticias = document.getElementsByClassName('noticia');
+    if (number == 1){
+
+        noticias[0].style.visibility = "visible";
+        noticias[0].style.gridArea = '1 / 2 / span 1 / span 1';
+        noticias[0].style.margin = '0 auto';
+
+        noticias[1].style.visibility = "visible";
+        noticias[1].style.gridArea = '1 / 3 / span 1 / span 1';
+        noticias[1].style.margin = '0 auto';
+
+        noticias[2].style.gridArea = '1 / 4 / span 1 / span 1';
+        noticias[2].style.margin = '0 auto';
+
+        noticias[3].style.gridArea = '1 / 5 / span 1 / span 1';
+        noticias[3].style.margin = '0 auto';
+
+        noticias[4].style.visibility = "hidden";
+        noticias[4].style.gridArea = '1 / 6 / span 1 / span 1';
+        noticias[4].style.margin = '0 auto';
+
+        noticias[5].style.visibility = "hidden";
+        noticias[5].style.gridArea = '1 / 0 / span 1 / span 1';
+        noticias[5].style.margin = '0 auto';
+    }
+    if (number == 2){
+
+        noticias[0].style.visibility = "hidden";
+        noticias[0].style.gridArea = '1 / 1 / span 1 / span 1';
+        noticias[0].style.margin = '0 auto';
+
+        noticias[1].style.visibility = "visible";
+        noticias[1].style.gridArea = '1 / 2 / span 1 / span 1';
+        noticias[1].style.margin = '0 auto';
+
+        noticias[2].style.gridArea = '1 / 3 / span 1 / span 1';
+        noticias[2].style.margin = '0 auto';
+
+        noticias[3].style.gridArea = '1 / 4 / span 1 / span 1';
+        noticias[3].style.margin = '0 auto';
+
+        noticias[4].style.visibility = "visible";
+        noticias[4].style.gridArea = '1 / 5 / span 1 / span 1';
+        noticias[4].style.margin = '0 auto';
+
+        noticias[5].style.visibility = "hidden";
+        noticias[5].style.gridArea = '1 / 6 / span 1 / span 1';
+        noticias[5].style.margin = '0 auto';
+    }
+    if (number == 3){
+        noticias[0].style.visibility = "hidden";
+        noticias[0].style.gridArea = '1 / 6 / span 1 / span 1';
+        noticias[0].style.margin = '0 auto';
+
+        noticias[1].style.visibility = "hidden";
+        noticias[1].style.gridArea = '1 / 1 / span 1 / span 1';
+        noticias[1].style.margin = '0 auto';
+
+        noticias[2].style.gridArea = '1 / 2 / span 1 / span 1';
+        noticias[2].style.margin = '0 auto';
+
+        noticias[3].style.gridArea = '1 / 3 / span 1 / span 1';
+        noticias[3].style.margin = '0 auto';
+
+        noticias[4].style.visibility = "visible";
+        noticias[4].style.gridArea = '1 / 4 / span 1 / span 1';
+        noticias[4].style.margin = '0 auto';
+
+        noticias[5].style.visibility = "visible";
+        noticias[5].style.gridArea = '1 / 5 / span 1 / span 1';
+        noticias[5].style.margin = '0 auto';
+    }
 }
