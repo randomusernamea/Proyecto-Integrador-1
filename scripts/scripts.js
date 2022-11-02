e30fcdc175a0024873b952896f92cd7653adb57c
randomusernamea <stars.a@outlook.com>
2022-11-01 22:29:03 -0300

    Responsive mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,30 +27,31 @@ function ingresarCerrar(){
 initializeNoticias();
 
 function initializeNoticias(){
-    noticias = document.getElementsByClassName('noticia');
-    agregarFechas();
-    noticias[0].style.visibility = "visible";
-    noticias[0].style.gridArea = '1 / 2 / span 1 / span 1';
-    noticias[0].style.margin = '0 auto';
+    if (document.documentElement.clientWidth> 500){
+        noticias = document.getElementsByClassName('noticia');
+        agregarFechas();
+        noticias[0].style.visibility = "visible";
+        noticias[0].style.gridArea = '1 / 2 / span 1 / span 1';
+        noticias[0].style.margin = '0 auto';
 
-    noticias[1].style.visibility = "visible";
-    noticias[1].style.gridArea = '1 / 3 / span 1 / span 1';
-    noticias[1].style.margin = '0 auto';
+        noticias[1].style.visibility = "visible";
+        noticias[1].style.gridArea = '1 / 3 / span 1 / span 1';
+        noticias[1].style.margin = '0 auto';
 
-    noticias[2].style.gridArea = '1 / 4 / span 1 / span 1';
-    noticias[2].style.margin = '0 auto';
+        noticias[2].style.gridArea = '1 / 4 / span 1 / span 1';
+        noticias[2].style.margin = '0 auto';
 
-    noticias[3].style.gridArea = '1 / 5 / span 1 / span 1';
-    noticias[3].style.margin = '0 auto';
+        noticias[3].style.gridArea = '1 / 5 / span 1 / span 1';
+        noticias[3].style.margin = '0 auto';
 
-    noticias[4].style.visibility = "hidden";
-    noticias[4].style.gridArea = '1 / 6 / span 1 / span 1';
-    noticias[4].style.margin = '0 auto';
+        noticias[4].style.visibility = "hidden";
+        noticias[4].style.gridArea = '1 / 6 / span 1 / span 1';
+        noticias[4].style.margin = '0 auto';
 
-    noticias[5].style.visibility = "hidden";
-    noticias[5].style.gridArea = '1 / 0 / span 1 / span 1';
-    noticias[5].style.margin = '0 auto';
-
+        noticias[5].style.visibility = "hidden";
+        noticias[5].style.gridArea = '1 / 0 / span 1 / span 1';
+        noticias[5].style.margin = '0 auto';
+    }
     document.getElementById('button1').onclick =function() {switchNoticias(1)};
 document.getElementById('button2').onclick =function() {switchNoticias(2)};
 document.getElementById('button3').onclick =function() {switchNoticias(3)};
